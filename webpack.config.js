c519dee20a1c9b3e31648b80dc1711eeb435f57c
SimoneSalzano <salzanosimone99@gmail.com>
2023-04-26 21:54:36 +0200

    Initialize Project environment

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("./node_modules/html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	mode: "development",
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "WebPage Name",
+			template: "./src/index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: ["node_modules"],
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	resolve: {
+		alias: {
+			config$: "./configs/app-config.js",
+			react: "./vendor/react-master",
+		},
+		extensions: [".js", ".jsx"],
+		modules: [
+			"node_modules",
+			"bower_components",
+			"shared",
+			"/shared/vendor/modules",
+		],
+	},
+};
