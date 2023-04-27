b023a1e5167f099e9f24e599d4662c87984c1c90
Simone Salzano <salzanosimone99@gmail.com>
2023-04-27 14:03:02 +0200

    Refactor placeShip to get length instead of Ship
    This way future refactoring of the class Ship will be easier.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,5 @@
+import Ship from "./Ship";
+
 const Gameboard = (size) => {
 	const shots = Array(size);
 	const shipPos = Array(size);
@@ -6,32 +8,34 @@ const Gameboard = (size) => {
 		shipPos[i] = Array(size);
 	}
 
-	const placeShip = (ship, startPos, horizontally = false) => {
+	const placeShip = (ofLength, startPos, horizontally = false) => {
 		const [y, x] = startPos;
 		if (x < 0 || x >= size || y < 0 || y >= size)
 			throw new RangeError(
 				`Position ${startPos} is out of bound for a board of size ${size}.`
 			);
 		if (horizontally) {
-			if (x + ship.length > size) {
+			if (x + ofLength > size) {
 				throw new RangeError(
-					`Ship of length ${ship.length} can't be horizontally positioned at (${x},${y})`
+					`Ship of length ${ofLength} can't be horizontally positioned at (${x},${y})`
 				);
 			}
-			for (let i = 0; i < ship.length; i += 1)
+			const ship = Ship(ofLength);
+			for (let i = 0; i < ofLength; i += 1)
 				if (shipPos[y][x + i])
 					throw new Error(`Another ship is already placed at (${x},${y})`);
-			for (let i = 0; i < ship.length; i += 1) shipPos[y][x + i] = ship;
+			for (let i = 0; i < ofLength; i += 1) shipPos[y][x + i] = ship;
 		} else {
-			if (y + ship.length > size) {
+			if (y + ofLength > size) {
 				throw new RangeError(
-					`Ship of length ${ship.length} can't be vertically positioned at (${x},${y})`
+					`Ship of length ${ofLength} can't be vertically positioned at (${x},${y})`
 				);
 			}
-			for (let i = 0; i < ship.length; i += 1)
+			const ship = Ship(ofLength);
+			for (let i = 0; i < ofLength; i += 1)
 				if (shipPos[y + i][x])
 					throw new Error(`Another ship is already placed at (${x},${y})`);
-			for (let i = 0; i < ship.length; i += 1) shipPos[y + i][x] = ship;
+			for (let i = 0; i < ofLength; i += 1) shipPos[y + i][x] = ship;
 		}
 	};
 
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -1,5 +1,4 @@
 import Gameboard from "../src/Gameboard";
-import Ship from "../src/Ship";
 
 let gm;
 
@@ -8,32 +7,35 @@ beforeEach(() => {
 });
 
 test("Ship gets placed only in a valid spot", () => {
-	const ship = Ship(3);
-	expect(() => gm.placeShip(ship, [-4, 0])).toThrow(RangeError);
-	expect(() => gm.placeShip(ship, [7, 7], true)).toThrow(RangeError);
-	expect(() => gm.placeShip(ship, [6, 5], true)).not.toThrow(RangeError);
+	const length = 3;
+	expect(() => gm.placeShip(length, [-4, 0])).toThrow(RangeError);
+	expect(() => gm.placeShip(length, [7, 7], true)).toThrow(RangeError);
+	expect(() => gm.placeShip(length, [6, 5], true)).not.toThrow(RangeError);
 });
 
 test("Ship gets placed properly", () => {
-	const ship = Ship(3);
-	gm.placeShip(ship, [0, 3], true);
-	expect(gm.shipPos[0][3]).toBe(ship);
-	expect(gm.shipPos[0][4]).toBe(ship);
-	expect(gm.shipPos[0][5]).toBe(ship);
-	expect(gm.shipPos[0][2]).not.toBe(ship);
-	expect(gm.shipPos[3][0]).not.toBe(ship);
-	gm.placeShip(ship, [5, 3]);
-	expect(gm.shipPos[5][3]).toBe(ship);
-	expect(gm.shipPos[6][3]).toBe(ship);
-	expect(gm.shipPos[7][3]).toBe(ship);
-	expect(gm.shipPos[4][3]).not.toBe(ship);
-	expect(gm.shipPos[5][2]).not.toBe(ship);
+	const length = 3;
+	gm.placeShip(length, [0, 3], true);
+	let placed = gm.shipPos[0][3];
+	expect(placed).toBeInstanceOf(Object);
+	expect(gm.shipPos[0][4]).toBe(placed);
+	expect(gm.shipPos[0][5]).toBe(placed);
+	expect(gm.shipPos[0][2]).not.toBe(placed);
+	expect(gm.shipPos[3][0]).not.toBe(placed);
+	gm.placeShip(length, [5, 3]);
+	// eslint-disable-next-line prefer-destructuring
+	placed = gm.shipPos[5][3];
+	expect(placed).toBeInstanceOf(Object);
+	expect(gm.shipPos[6][3]).toBe(placed);
+	expect(gm.shipPos[7][3]).toBe(placed);
+	expect(gm.shipPos[4][3]).not.toBe(placed);
+	expect(gm.shipPos[5][2]).not.toBe(placed);
 });
 
 test("Can't place a ship in a location where another ship is placed", () => {
-	const ship = Ship(3);
-	gm.placeShip(ship, [0, 0], false);
-	expect(() => gm.placeShip(Ship(2), [2, 0], true)).toThrow(
+	const length = 3;
+	gm.placeShip(length, [0, 0], false);
+	expect(() => gm.placeShip(length, [2, 0], true)).toThrow(
 		"Another ship is already placed at (0,2)"
 	);
 });
