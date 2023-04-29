31dad51f4c5db0f6715750ceb94c53219aa4418a
SimoneSalzano <salzanosimone99@gmail.com>
2023-04-29 14:19:54 +0200

    Implement ship visualization on the boards

---
+++ src/DOM.js
@@ -0,0 +1,26 @@
+const DOM = (() => {
+	const board1 = document.getElementById("board-1");
+	const board2 = document.getElementById("board-2");
+
+	const createCell = (ofBoard, x, y) => {
+		const cell = document.createElement("div");
+		cell.classList.add("cell");
+		cell.dataset.x = x;
+		cell.dataset.y = y;
+		if (ofBoard.shipPos[y][x]) cell.classList.add("ship");
+		return cell;
+	};
+
+	const initializeBoards = (ofSize, b1, b2) => {
+		for (let i = 0; i < ofSize; i += 1) {
+			for (let j = 0; j < ofSize; j += 1) {
+				board1.appendChild(createCell(b1, i, j));
+				board2.appendChild(createCell(b2, i, j));
+			}
+		}
+	};
+
+	return { initializeBoards };
+})();
+
+export default DOM;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import "./style.scss";
 import Gameboard from "./Gameboard";
 import Player from "./Player";
+import DOM from "./DOM";
 
 const initialize = (boardSize, name1, name2, againstAI = true) => {
 	const g1 = Gameboard(boardSize);
 	const g2 = Gameboard(boardSize);
-	const shipLengths = [5, 4, 3, 2, 2, 1];
+	const shipLengths = [5, 4, 3, 3, 2];
 	g1.placeRandomly(shipLengths);
 	g2.placeRandomly(shipLengths);
 	const p1 = Player(name1, g1, g2, false);
 	const p2 = Player(name2, g2, g1, againstAI);
+	DOM.initializeBoards(boardSize, g1, g2);
 	return { p1, p2 };
 };
 
