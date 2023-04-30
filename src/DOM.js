17785def805b455ac9be1eabcf61040090565bde
SimoneSalzano <salzanosimone99@gmail.com>
2023-04-30 17:30:07 +0200

    Remove DOM-Player coupling

--- src/DOM.js
+++ src/DOM.js
@@ -1,6 +1,6 @@
 import "./style.scss";
 
-const DOM = (p1, p2) => {
+const DOM = (g1, g2) => {
 	const board1 = document.getElementById("board-1");
 	const board2 = document.getElementById("board-2");
 	const endgameModal = document.getElementById("endgame");
@@ -56,15 +56,14 @@ const DOM = (p1, p2) => {
 		}
 	};
 
-	const visualizeShot = (player, result, x, y) => {
-		const board = player === p1 ? board2 : board1;
+	const visualizeShot = (isPlayer1, result, x, y) => {
+		const board = isPlayer1 ? board2 : board1;
 		const target = board.querySelector(`[data-x="${x}"][data-y="${y}"]`);
 		const pin = document.createElement("div");
 		if (result) {
 			if (result === "hit" || result === "sunk") pin.classList.add("hit");
 			else pin.classList.add("miss");
-			if (player === p1 && result === "sunk")
-				visualizeShip(p1.enemyGameboard, x, y);
+			if (isPlayer1 && result === "sunk") visualizeShip(g2, x, y);
 			target.appendChild(pin);
 		}
 	};
@@ -73,7 +72,7 @@ const DOM = (p1, p2) => {
 		if (board === board2) {
 			const { x, y } = target.dataset;
 			const result = attackAction(x, y);
-			visualizeShot(p1, result, x, y);
+			visualizeShot(true, result, x, y);
 			if (result) attackFollowUp();
 		}
 	};
@@ -84,7 +83,7 @@ const DOM = (p1, p2) => {
 		cell.dataset.x = x;
 		cell.dataset.y = y;
 		const ship = ofBoard.shipPos[y][x];
-		if (ofBoard === p1.ownGameboard && ship) {
+		if (ofBoard === g1 && ship) {
 			cell.classList.add(`ship${(ship.id % 5) + 1}`);
 			cell.classList.add(`ship`);
 		}
@@ -100,8 +99,8 @@ const DOM = (p1, p2) => {
 	const initializeBoards = (ofSize) => {
 		for (let i = 0; i < ofSize; i += 1) {
 			for (let j = 0; j < ofSize; j += 1) {
-				board1.appendChild(createCell(p1.ownGameboard, j, i));
-				board2.appendChild(createCell(p2.ownGameboard, j, i));
+				board1.appendChild(createCell(g1, j, i));
+				board2.appendChild(createCell(g2, j, i));
 			}
 		}
 	};
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const initialize = (boardSize, name1, name2, againstAI = true) => {
 	g2.placeRandomly(shipLengths);
 	const p1 = Player(name1, g1, g2, false);
 	const p2 = Player(name2, g2, g1, againstAI);
-	const UI = DOM(p1, p2);
+	const UI = DOM(g1, g2);
 	UI.attackAction = (x, y) => {
 		if (!p1.ownGameboard.isAllSunk() && !p1.enemyGameboard.isAllSunk())
 			return p1.attack(x, y);
@@ -19,7 +19,7 @@ const initialize = (boardSize, name1, name2, againstAI = true) => {
 	UI.attackFollowUp = () => {
 		if (!p2.ownGameboard.isAllSunk()) {
 			const { x, y, result } = p2.makeMove();
-			UI.visualizeShot(p2, result, x, y);
+			UI.visualizeShot(false, result, x, y);
 			if (p1.ownGameboard.isAllSunk())
 				UI.displayEndMessage("Oh no! You lost all your ships!");
 		} else {
